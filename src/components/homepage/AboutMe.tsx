2c00f83e2e095d0230cce7db767f23985e22dd16
Nathan Dalpatan <nathandalp@gmail.com>
2025-10-25 20:17:18 +1300

    rookie mistake

--- src/components/homepage/AboutMe.tsx
+++ src/components/homepage/AboutMe.tsx
@@ -5,7 +5,7 @@ export default function AboutMe() {
   return (
     <div className="min-h-screen flex flex-col items-center justify-center gap-2">
       <p className="text-center">
-        Hi I'm Nathan, a computer science and IT management student at the
+        Hi I&apos;m Nathan, a computer science and IT management student at the
         University of Auckland.
         <br /> Outside of school, I like to stay active and work on not sucking
         at baking...
